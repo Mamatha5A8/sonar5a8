28359a2efc891c6f7d53806036e8628834b1392d
Mamatha <Mamatha@LAPTOP-7J9ULA4Q>
2022-11-28 14:53:21 +0530

    hii

---
+++ src/main/java/acoecse2/cseb5a82/App.java
@@ -0,0 +1,13 @@
+package acoecse2.cseb5a82;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );;
+    }
+}
